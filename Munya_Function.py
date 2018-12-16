e1b69188767c45f12287185080a7d76a24ee8be5
mnyakays <mnya.kays@gmail.com>
2018-12-16 13:00:25 +0200

    my first git commit

---
+++ Munya_Function.py
@@ -0,0 +1,28 @@
+# #global variable
+# global_name = "tinashe"
+#
+# def make_full_name(first_name = "",last_name = ""):
+#     """
+#     Make full name  for the first name and the last name
+#     Example:
+#          make_full_name("Munya", "Kadyamurandu")
+#     Output:
+#         Munya Kadyamurandu
+#
+#     """
+#     if len(first_name) < 0  and len (last_name) < 0:
+#         print("please get a better name!")
+#     elif 'a' in first_name:
+#         print("a is allowed")
+#     full_name = first_name.capitalize() + last_name.capitalize()
+#     return full_name
+#
+#
+# short = global_name[0:3]
+# def to_shortname():
+#     short_name = global_name[0:3]
+#     return short_name
+# print(short)
+# print(to_shortname())
+
+
